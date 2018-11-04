5df6b1ea6835b8c7c1aac5fc883862c454731b8a
Lokiiiiii <loki94@cmu.edu>
2018-11-04 18:48:26 -0500

    working code

--- models/3DboxNet_v1.py
+++ models/3DboxNet_v1.py
@@ -1,14 +1,15 @@
 import torch.nn as nn
 import torch
 import time
+import numpy as np
+import globalVariables as glb
 
-NUM_HEADING_BIN =
-NUM_SIZE_CLUSTER=
+NUM_HEADING_BIN = glb.NUM_HEADING_BIN
+NUM_SIZE_CLUSTER= glb.NUM_SIZE_CLUSTER
 
 class Model(nn.Module):
-    def __init__(num_in_channels, num_pool, num_input_to_fc, activation=nn.ReLU, bn_decay=0):
+    def __init__(num_in_channels=1, num_input_to_fc=(512+10), activation=nn.ReLU, bn_decay=0):
         super().__init__()
-        self.num_point=num_pool
         self.Activation = activation
         self.cnn1 = nn.Sequential(
                                 nn.Conv2d(num_in_channels, 128, kernel_size=(1,1), stride=(1,1), padding=(0,0), bias=True), #32 bit floats in original code. padding was VALID
@@ -30,7 +31,6 @@ class Model(nn.Module):
                                 nn.BatchNorm2d(512, momentum=1-bn_decay), #No momentum in original code.
                                 self.Activation()
                                 )
-        self.pool = nn.MaxPool2d(kernel_size=(self.num_point,1), stride=(2,2), padding=0) #padding was VALID
         self.fc1  = nn.Sequential(
                                 nn.Linear(num_input_to_fc, 512, bias=True),
                                 nn.BatchNorm1d(512, momentum=1-bn_decay),
@@ -47,22 +47,43 @@ class Model(nn.Module):
         self.fc3  = nn.Linear(256, 3+NUM_HEADING_BIN*2+NUM_SIZE_CLUSTER*4, bias=True)
         self.layers = [self.cnn1, self.cnn2, self.cnn3, self.cnn4, self.fc1, self.fc2, self.fc3]
         self.initializeWeights()
-                                
+        self.end_points = {}
+        self.mean_size_arr = np.zeros((NUM_SIZE_CLUSTER, 3))                                
 
 
-    def forward(self, object_point_cloud, one_hot_vec):
-        x = object_point_cloud.view(object_point_cloud.shape[0],object_point_cloud.shape[1],1,object_point_cloud.shape[2])
+    def forward(self, object_point_cloud, mask, stage1_center, one_hot_vec):
+        self.end_points['stage1_center'] = stage1_center
+        point_cloud_xyz_submean = object_point_cloud - stage1_center.unsqueeze(1)
+        x = point_cloud_xyz_submean.unsqueeze(2)
+        x = x.view(x.shape[0],x.shape[1],1,x.shape[2])
         x = self.cnn1(x)
         x = self.cnn2(x)
         x = self.cnn3(x)
         x = self.cnn4(x)
-        x = self.pool(x)
+        mask_expand = torch.repeat(mask.unsqueeze(-1), [1,1,1,512])
+        masked_x = x*mask_expand
+        x = nn.functional.max_pool2d(masked_x, kernel_size=(object_point_cloud.size(1),1), stride=(2,2), padding=0)
         x = torch.squeeze(x, dim=[1,2])
         x = torch.cat([x,one_hot_vec], 1)
         x = self.fc1(x)
         x = self.fc2(x)
         x = self.fc3(x)
-        return x
+        center = x[:,0:3] + stage1_center
+        self.end_points['center']=center
+        heading_scores = x[:,3:NUM_HEADING_BIN]
+        heading_residuals_normalized = x[:3+NUM_HEADING_BIN,NUM_HEADING_BIN]
+        self.end_points['heading_scores'] = heading_scores # BxNUM_HEADING_BIN
+        self.end_points['heading_residuals_normalized'] = heading_residuals_normalized # BxNUM_HEADING_BIN (should be -1 to 1)
+        self.end_points['heading_residuals'] = heading_residuals_normalized * (np.pi/NUM_HEADING_BIN) # BxNUM_HEADING_BIN
+    
+        size_scores = x[:,3+NUM_HEADING_BIN*2:NUM_SIZE_CLUSTER] # BxNUM_SIZE_CLUSTER
+        size_residuals_normalized = x[:,3+NUM_HEADING_BIN*2+NUM_SIZE_CLUSTER:NUM_SIZE_CLUSTER*3]
+        size_residuals_normalized = size_residuals_normalized.view([batch_size, NUM_SIZE_CLUSTER, 3]) # BxNUM_SIZE_CLUSTERx3
+        end_points['size_scores'] = size_scores
+        end_points['size_residuals_normalized'] = size_residuals_normalized
+        end_points['size_residuals'] = size_residuals_normalized * self.mean_size_arr.unsqueeze(0)
+        return end_points
+
 
     def initializeWeights(self, function=nn.init.xavier_normal_):
         for layer in self.layers:
--- models/ThreeDboxNet_v1.py
+++ models/ThreeDboxNet_v1.py
@@ -8,7 +8,7 @@ NUM_HEADING_BIN = glb.NUM_HEADING_BIN
 NUM_SIZE_CLUSTER= glb.NUM_SIZE_CLUSTER
 
 class Model(nn.Module):
-    def __init__(num_in_channels=1, num_input_to_fc, activation=nn.ReLU, bn_decay=0):
+    def __init__(self,num_in_channels=3, num_input_to_fc=(512+glb.NUM_CLASS), activation=nn.ReLU, bn_decay=0):
         super().__init__()
         self.Activation = activation
         self.cnn1 = nn.Sequential(
@@ -48,22 +48,21 @@ class Model(nn.Module):
         self.layers = [self.cnn1, self.cnn2, self.cnn3, self.cnn4, self.fc1, self.fc2, self.fc3]
         self.initializeWeights()
         self.end_points = {}
-        self.mean_size_arr = np.zeros((NUM_SIZE_CLUSTER, 3))                                
+        self.mean_size_arr = glb.mean_size_arr                                
 
 
     def forward(self, object_point_cloud, mask, stage1_center, one_hot_vec):
         self.end_points['stage1_center'] = stage1_center
         point_cloud_xyz_submean = object_point_cloud - stage1_center.unsqueeze(1)
-        x = point_cloud_xyz_submean.unsqueeze(2)
-        x = x.view(x.shape[0],x.shape[1],1,x.shape[2])
+        x = point_cloud_xyz_submean.permute(0,2,1).unsqueeze(3)
         x = self.cnn1(x)
         x = self.cnn2(x)
         x = self.cnn3(x)
         x = self.cnn4(x)
-        mask_expand = torch.repeat(mask.unsqueeze(-1), [1,1,1,512])
-        masked_x = x*mask_expand
+        mask_expand = mask.unsqueeze(-1).repeat(1,1,1,512)
+        masked_x = x*mask_expand.float().permute(0,3,1,2)
         x = nn.functional.max_pool2d(masked_x, kernel_size=(object_point_cloud.size(1),1), stride=(2,2), padding=0)
-        x = torch.squeeze(x, dim=[1,2])
+        x = x.squeeze(-1).squeeze(-1)
         x = torch.cat([x,one_hot_vec], 1)
         x = self.fc1(x)
         x = self.fc2(x)
@@ -76,13 +75,13 @@ class Model(nn.Module):
         self.end_points['heading_residuals_normalized'] = heading_residuals_normalized # BxNUM_HEADING_BIN (should be -1 to 1)
         self.end_points['heading_residuals'] = heading_residuals_normalized * (np.pi/NUM_HEADING_BIN) # BxNUM_HEADING_BIN
     
-        size_scores = x[:,3+NUM_HEADING_BIN*2:NUM_SIZE_CLUSTER] # BxNUM_SIZE_CLUSTER
-        size_residuals_normalized = x[:,3+NUM_HEADING_BIN*2+NUM_SIZE_CLUSTER:NUM_SIZE_CLUSTER*3]
-        size_residuals_normalized = size_residuals_normalized.view([batch_size, NUM_SIZE_CLUSTER, 3]) # BxNUM_SIZE_CLUSTERx3
-        end_points['size_scores'] = size_scores
-        end_points['size_residuals_normalized'] = size_residuals_normalized
-        end_points['size_residuals'] = size_residuals_normalized * self.mean_size_arr.unsqueeze(0)
-        return end_points
+        size_scores = x[:,3+NUM_HEADING_BIN*2:NUM_SIZE_CLUSTER+3+NUM_HEADING_BIN*2+1] # BxNUM_SIZE_CLUSTER
+        size_residuals_normalized = x[:,3+NUM_HEADING_BIN*2+NUM_SIZE_CLUSTER:NUM_SIZE_CLUSTER*3+3+NUM_HEADING_BIN*2+NUM_SIZE_CLUSTER+1]
+        size_residuals_normalized = size_residuals_normalized.view([object_point_cloud.size(0), NUM_SIZE_CLUSTER, 3]) # BxNUM_SIZE_CLUSTERx3
+        self.end_points['size_scores'] = size_scores
+        self.end_points['size_residuals_normalized'] = size_residuals_normalized
+        self.end_points['size_residuals'] = size_residuals_normalized * torch.FloatTensor(self.mean_size_arr).unsqueeze(0)
+        return self.end_points
 
 
     def initializeWeights(self, function=nn.init.xavier_normal_):
