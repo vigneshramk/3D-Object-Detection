1987fe8046a0d97b201fb63509e441497be8f848
vigneshramk <ram7sachin@gmail.com>
2018-11-06 19:10:39 -0500

    merge
    Signed-off-by: vigneshramk <ram7sachin@gmail.com>

--- train/hyperParams.py
+++ train/hyperParams.py
@@ -5,13 +5,13 @@ hyp = {
 "lr": 1e-4,
 "batch_size": 90,
 "num_workers":1,
-"grad_clip": 5,
+"grad_clip": 0.5,
 "optim_reg": 1e-5,
 "model_fname": "Train_v1",
 "hyp_fname": "Hyp_v1",
 "log_freq": 1,
 "gpu":"5,6,7",
-"log_dir":"test_noval",
-"test_name":"test_noval",
+"log_dir":"test_clip",
+"test_name":"test_clip",
 "parallel":False
 }
