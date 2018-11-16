10a1a3039767621348af1974c7cb2cc1c917b9ff
vigneshramk <ram7sachin@gmail.com>
2018-11-15 21:26:22 -0500

    val loader done
    Signed-off-by: vigneshramk <ram7sachin@gmail.com>

--- data/loader.py
+++ data/loader.py
@@ -3,9 +3,9 @@ import gzip
 import numpy as np
 
 # Make a folder data inside the 3D-Object-Detection folder and put the pickle file inside it
-# SUN_PATH = '/home/ubuntu/3D-Object-Detection/data'
-SUN_PATH = './../data'
-SUN_TRAIN_FILE = 'sunrgbd_3d.pickle'
+SUN_PATH = '/home/ubuntu/3D-Object-Detection/data'
+# SUN_PATH = './../data'
+SUN_TRAIN_FILE = 'sunrgbd_train.pickle'
 SUN_VAL_FILE = 'sunrgbd_val.pickle'
 
 type2class={'bed':0, 'table':1, 'sofa':2, 'chair':3, 'toilet':4, 'desk':5, 'dresser':6, 'night_stand':7, 'bookshelf':8, 'bathtub':9}
---
+++ data/split_data.py
@@ -0,0 +1,49 @@
+from loader import SUNRGBD
+import numpy as np
+import cv2
+import os
+import pickle
+import gzip
+
+def save_zipped_pickle(obj, filename, protocol=-1):
+    with gzip.open(filename, 'wb') as f:
+        pickle.dump(obj, f, protocol)
+
+def load_zipped_pickle(filename):
+    with gzip.open(filename, 'rb') as f:
+        loaded_object = pickle.load(f)
+        return loaded_object
+
+def split_dataset():
+    train_factor = 0.7
+    loader = SUNRGBD()
+    id_list,box2d_list,box3d_list,input_list,label_list,type_list,heading_list,size_list,frustum_angle_list=loader.train
+    data_size = len(id_list)
+    indices = np.arange(0,data_size)
+    np.random.shuffle(indices)
+    train_size = int(train_factor*data_size)
+    val_size = int((1-train_factor)*data_size)
+
+    ti = indices[:train_size]
+    vi = indices[train_size:train_size+val_size]
+
+    ti = ti.astype(int).tolist()
+    vi = vi.astype(int).tolist()
+
+    id_list = np.array(id_list)
+    box2d_list = np.array(box2d_list)
+    box3d_list = np.array(box3d_list)
+    input_list = np.array(input_list)
+    label_list = np.array(label_list)
+    type_list = np.array(type_list)
+    heading_list = np.array(heading_list)
+    size_list = np.array(size_list)
+    frustum_angle_list = np.array(frustum_angle_list)
+
+
+    save_zipped_pickle([id_list[ti].tolist(),box2d_list[ti].tolist(),box3d_list[ti].tolist(),input_list[ti].tolist(),label_list[ti].tolist(),type_list[ti].tolist(),heading_list[ti].tolist(),size_list[ti].tolist(),frustum_angle_list[ti].tolist()],'sunrgbd_train.pickle')
+    save_zipped_pickle([id_list[vi].tolist(),box2d_list[vi].tolist(),box3d_list[vi].tolist(),input_list[vi].tolist(),label_list[vi].tolist(),type_list[vi].tolist(),heading_list[vi].tolist(),size_list[vi].tolist(),frustum_angle_list[vi].tolist()],'sunrgbd_val.pickle')
+
+split_dataset()
+
+
